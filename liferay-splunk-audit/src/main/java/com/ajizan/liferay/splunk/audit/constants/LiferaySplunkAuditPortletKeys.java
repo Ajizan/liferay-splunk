4fe44dfb5be582dc5d4880c47ebfba3d59485787
harrari <om.harrari@gmail.com>
2018-11-23 14:36:46 +0100

    move splunk-audit to other module

---
+++ liferay-splunk-audit/src/main/java/com/ajizan/liferay/splunk/audit/constants/LiferaySplunkAuditPortletKeys.java
@@ -0,0 +1,10 @@
+package com.ajizan.liferay.splunk.audit.constants;
+
+/**
+ * @author oharrari
+ */
+public class LiferaySplunkAuditPortletKeys {
+
+	public static final String LiferaySplunkAudit = "liferaysplunkaudit";
+
+}
---
+++ liferay-splunk-audit/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="liferaysplunkaudit.caption"/></b>
+</p>
